378ff06bb5154e6b832e7d5221a1367cfb4d9bdb
porfiriovitin <victorhugo.svm@hotmail.com>
2026-05-06 22:35:41 -0300

    feat: structuring the controller with a default response type

---
+++ src/Backend/MyRecipeBook.Application/UseCases/User/RegisterUserUseCase.cs
@@ -0,0 +1,31 @@
+﻿using MyRecipeBook.Communication.Enums;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+
+namespace MyRecipeBook.Application.UseCases;
+
+public class RegisterUserUseCase
+{
+    public PayloadResponse<ResponseRegisteredUserJson> Execute(RequestRegisterUserJson request)
+    {
+
+        /// :: Validate the request.
+
+
+        /// :: Maps the request to the domain model.
+
+
+        /// :: Hash the password.
+
+
+        /// :: Save the user to the database.
+
+
+        return new PayloadResponse<ResponseRegisteredUserJson>
+        {
+            Status = nameof(ResponseStatus.Success),
+            Message = "Usuário cadastrado com sucesso",
+            Data = new ResponseRegisteredUserJson(request.Name, request.Email)
+        };
+    }
+}
